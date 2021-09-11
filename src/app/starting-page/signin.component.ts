7a09d4177cbbae475a454b0f806be54d0c844f8a
Angella Falck <angella.falck@unitec.edu>
2021-09-11 00:29:11 -0600

    Cambios agregados

--- src/app/starting-page/signin.component.ts
+++ src/app/starting-page/signin.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { ToastrService } from "../common/toastr.service";
 import { users } from "../models/pizzeria.model";
 import { PizzeriaService } from "../services/pizzeria.service";
 
 @Component({
+    selector: 'signin',
     templateUrl: './signin.component.html',
     styles: [ `
 
@@ -16,7 +18,8 @@ export class SigninComponent{
     user: users;
     userLength: number = 0;
 
-    constructor(private pizzeriaService: PizzeriaService, private router: Router){
+    constructor(private pizzeriaService: PizzeriaService, private router: Router,
+        private toastrService: ToastrService){
     }
 
     fnSigin(dataForm: any)
@@ -31,7 +34,7 @@ export class SigninComponent{
     {
         if(this.user[0].length == 0) //validar que no exista el correo
         {
-            alert("Cuenta creada exitosamente");
+            this.toastrService.success("Sign In","Cuenta creada exitosamente");
             console.log(data);
             try{
                 this.pizzeriaService.postUser(data).subscribe((response) => {
@@ -44,7 +47,7 @@ export class SigninComponent{
         } 
         else
         {
-            alert("Correo ya pertenece a una cuenta existente");
+            this.toastrService.error("Error","Correo ya pertenece a una cuenta existente");
         }
     }
 
